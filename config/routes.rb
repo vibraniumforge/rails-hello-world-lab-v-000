ded5340b6d377496b5e6a7cfe33cf0df51ff5ef8
Greg Morgan <gregm9999@gmail.com>
2018-12-20 22:13:18 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+    get 'hello_world', to: 'static#hello_world'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,5 +54,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  get 'hello_world', to: 'static#hello_world'
+
 end
